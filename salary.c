fb5c6664a8c63b05a4ba019ac35c297c118c4301
Srbd006 <bhaskartejatankala006@gmail.com>
2024-09-11 11:29:56 +0530

    Add files via upload

---
+++ salary.c
@@ -0,0 +1,34 @@
+//to calculate gross salary
+#include<stdio.h>
+int main()
+{
+   float b,a,bo,ov,gr;
+   printf("enter your basic salary");
+   scanf("%f",&b);
+    printf("enter your allowances");
+   scanf("%f",&a);
+    printf("enter your bonus ");
+   scanf("%f",&bo);
+    printf("enter your over time");
+   scanf("%f",&ov);
+   gr=b+a+bo+ov;
+   printf("your gross salary=%f",gr);
+   }
+   
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
